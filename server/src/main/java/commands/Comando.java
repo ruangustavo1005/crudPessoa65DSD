da789ccb05ff03085d8500ca2a8623abc4172d73
Leonardo Alex Fusinato <leonardo.alex.fusinato@gmail.com>
2021-10-24 13:49:36 -0300

    finalization

--- server/src/main/java/commands/Comando.java
+++ server/src/main/java/commands/Comando.java
@@ -2,12 +2,6 @@ package commands;
 
 abstract public class Comando {
     
-    public static final String INSERT = "INSERT";
-    public static final String DELETE = "DELETE";
-    public static final String GET    = "GET";
-    public static final String UPDATE = "UPDATE";
-    public static final String LIST = "LIST";
-    
     protected boolean success;
     
     public abstract void execute(String[] args);
